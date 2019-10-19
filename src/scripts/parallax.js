6e42d0cc80e1ab80abecb3baedce57e3077e8e41
maxnhub <maxnikulnikov@gmail.com>
2019-10-19 16:53:35 +0300

    parallax to 1200

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -1,10 +1,10 @@
 const parallax = document.querySelector('.main');
-const layers = main.children;
+const layers = parallax.children;
 
 function moveLayersDependsOnScroll(wScroll) {
     Array.from(layers).forEach(layer => {
         const divider = layer.dataset.speed;
-        const strafe = wScroll * divider / 10;
+        const strafe = wScroll * divider / 20;
         layer.style.transform = `translateY(-${strafe}%)`;
     })
 }
